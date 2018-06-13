fc3b990a6b0d2aa03933ee48df40433abfdbc086
ishikaaa <gargishika1998>
2018-06-13 14:15:12 +0530

    Assignment-2 completed

---
+++ Assognment-2.py
@@ -0,0 +1,25 @@
+#Q1.
+print("Ishika Garg")
+
+#Q2.
+print("Acad"+"View")
+
+#Q3.
+a=input("Enter 1st number=")
+b=input("Enter 2nd number=")
+c=input("Enter 3rd number=")
+print("a="+a+" b="+b+" c="+c)
+
+#Q4.
+print('Let\'s get started')
+
+#Q5.
+s="Acadview"
+course="Python"
+fees=5000
+print("s=%s\ncourse=%s\nfees=%d"%(a,course,fees))
+
+#Q6.
+name="Tony Stark"
+salary=1000000
+print('%s"%d'%(name,salary))
